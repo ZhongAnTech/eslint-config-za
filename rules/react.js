c0f72116e011712303585fe3b32829398bb8cd3d
Lin Qun <jinasonlin@gmail.com>
2018-05-10 15:31:46 +0800

    fixed: jsx-closing

--- rules/react.js
+++ rules/react.js
@@ -3,7 +3,6 @@ module.exports = {
     'react/prefer-stateless-function': 'off',
     'react/prop-types': 'off',
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
-    'react/jsx-closing-bracket-location': ['error', { selfClosing: 'props-aligned', nonEmpty: 'after-props' }],
     'react/no-array-index-key': 'warn',
   },
 };
