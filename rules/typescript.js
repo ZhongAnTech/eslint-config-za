a2db611cd5da96e976bcb9ebd0a2686b330f9081
Lin Qun <jinasonlin@gmail.com>
2019-03-29 16:42:28 +0800

    chore: update ts rules

--- rules/typescript.js
+++ rules/typescript.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: ['@typescript-eslint'],
   rules: {
-    // indent 暂时采用 ESLint 内建规则
+    // 暂时采用 ESLint 内建规则或编译
     // '@typescript-eslint/indent': [
     //   'error',
     //   2,
@@ -9,6 +9,12 @@ module.exports = {
     //     SwitchCase: 1,
     //   },
     // ],
+    // '@typescript-eslint/camelcase': 'off',
+    // '@typescript-eslint/no-array-constructor': 'off',
+    // '@typescript-eslint/no-unused-vars': 'off',
+    // '@typescript-eslint/no-useless-constructor': 'error',
+    // '@typescript-eslint/no-use-before-define': 'off',
+
     '@typescript-eslint/adjacent-overload-signatures': 'error',
     '@typescript-eslint/array-type': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
@@ -22,13 +28,12 @@ module.exports = {
     '@typescript-eslint/member-delimiter-style': 'error',
     '@typescript-eslint/member-naming': 'off',
     '@typescript-eslint/no-angle-bracket-type-assertion': 'error',
-    '@typescript-eslint/no-array-constructor': 'off',
     '@typescript-eslint/no-empty-interface': 'error',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-extraneous-class': 'off',
-    '@typescript-eslint/no-for-in-array': 'error',
+    '@typescript-eslint/no-for-in-array': 'off',
     '@typescript-eslint/no-inferrable-types': 'error',
-    '@typescript-eslint/no-misused-new': 'off',
+    '@typescript-eslint/no-misused-new': 'error',
     '@typescript-eslint/no-namespace': [
       'error',
       {
@@ -45,11 +50,6 @@ module.exports = {
     '@typescript-eslint/no-type-alias': 'off',
     '@typescript-eslint/no-unnecessary-qualifier': 'off',
     '@typescript-eslint/no-unnecessary-type-assertion': 'off',
-    // 编译代替
-    '@typescript-eslint/no-unused-vars': 'off',
-    // 编译代替
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/no-useless-constructor': 'error',
     '@typescript-eslint/no-var-requires': 'error',
     '@typescript-eslint/prefer-function-type': 'error',
     '@typescript-eslint/prefer-interface': 'error',
