ffbe7420f7a9454730234c0f32527a91923a95dc
Lin Qun <jinasonlin@gmail.com>
2019-05-15 14:22:49 +0800

    chore: update deps

--- test/typescript/test.ts
+++ test/typescript/test.ts
@@ -14,3 +14,15 @@ export class Class {
     console.info(this);
   }
 }
+
+// // eslint rule: arrow-parens
+// // typescript-eslint <= 1.9.0，目前对 arrow-parens 支持不足，待修复
+// const bar1 = <T>(t: T) => {
+//   console.info(t);
+// };
+
+// // eslint rule: space-before-function-paren
+// // typescript-eslint <= 1.9.0，目前对 space-before-function-paren 支持不足，待修复
+// const bar2 = function <T>(t: T) {
+//   console.info(t);
+// };
